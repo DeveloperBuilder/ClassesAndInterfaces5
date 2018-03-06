a3f1cdfad763aab0cfee8f63a32b5d83500ee30b
LSLIM <LSLIM@LS>
2018-03-06 21:33:20 +0100

    The syntax is complete

--- ClassesAndInterfaces5/Program.cs
+++ ClassesAndInterfaces5/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KinderBoerderij;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,36 @@ namespace ClassesAndInterfaces5
 {
     class Program
     {
+        /* Maak een voorbeeld console-applicatie KinderBoerderij, met daarin twee konijnen (knabbel en flappie). 
+           Een konijn heeft een leeftijd (int) en kan huppelen. Wanneer het konijn huppelt, wordt er tekst op het scherm geprint. 
+           Laat in de applicatie beide konijnen huppelen. */
+
         static void Main(string[] args)
         {
+            Konijn knabbel = new Konijn();
+            Konijn flappie = new Konijn();
+
+            knabbel.huppelen();
+            flappie.huppelen();
+            Console.ReadLine();
+        }
+        
+    }
+}
+
+
+        
+namespace KinderBoerderij
+{
+    class Konijn
+    {
+        private int leeftijd;
+
+        public void huppelen()
+        {
+            Console.WriteLine("Huppel Huppel");
         }
     }
 }
+
+
